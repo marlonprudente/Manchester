44247dd955c0f185c0ebf8cc237163f456d51b83
Marlon <marlonmateuspr@gmail.com>
2016-11-14 20:55:03 -0200

    Implementação classe BynaryConverter, agora ela converte texto para binario e binario para texto.

--- src/manchester/BinaryConverter.java
+++ src/manchester/BinaryConverter.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 public class BinaryConverter {
 
     public String BinaryToText(Integer[] binario) {
-        int letras = (binario.length/7);
+        int letras = (binario.length / 7);
         char c[] = new char[binario.length];
         String nome;
         int j = 0, k = 0, l = 1, m = 0;
@@ -23,48 +23,45 @@ public class BinaryConverter {
 
         while (k != binario.length) {
 
-            for(j = 0;j<=6;j++){                
+            for (j = 0; j <= 6; j++) {
                 op += binario[k] * l;
                 //System.out.println(binario[k] + "*" + l + "=" + op);
                 l = 2 * l;
                 k++;
             }
-            
+
             //System.out.println("OP = " + op);
-            c[m] = (char)op;
+            c[m] = (char) op;
             m++;
             op = 0;
             l = 1;
-  
+
         }
         //aqui estou invertendo a ordem da string
         int tam = c.length;
         char[] aux = new char[c.length];
-        for(int i = 0; i<aux.length;i++){
+        for (int i = 0; i < aux.length; i++) {
             tam--;
             aux[i] = c[tam];
         }
         //Aqui eu estou copiando so a parte que interessa do vetor
-        c = Arrays.copyOfRange(aux, binario.length-letras, binario.length);
+        c = Arrays.copyOfRange(aux, binario.length - letras, binario.length);
+        
         //aqui estou transformando o vetor de char em uma string
         nome = String.valueOf(c);
+        
         return nome;
-
     }
 
     public Integer[] textToBinary(String texto) {
-        int op = texto.length() * 7;
-        Integer bin;
+        int op = texto.length() * 7; //numero de characteres * numero de bits
+        Integer bin;//Aqui é necessário definir o tamanho para 8 caracteres (ainda não fiz)
         Integer[] array = new Integer[op];
         int j = 0, k = 0;
-        for (char c : texto.toCharArray()) {
-            //System.out.println(c + " = " + Integer.toBinaryString((c)));
-            //Aqui é possivel ver o numero binario gerado pela string
-            // System.out.println("Inteiro: " + Integer.parseInt(Integer.toBinaryString((c))));
 
+        for (char c : texto.toCharArray()) {
             //Agora e preciso dividir cada caractere em uma posicao do vetor para o manchester
             bin = Integer.parseInt(Integer.toBinaryString((c)));
-            System.out.println("Binario de " + c +"= "+ bin);
             //Aqui estou transformando em um vetorzão com todos os 0's e 1's
             while (j != 7) {
                 array[k] = Character.getNumericValue(bin.toString().charAt(j));
@@ -77,7 +74,7 @@ public class BinaryConverter {
         //aqui estou invertendo a ordem da string
         int tam = array.length;
         Integer[] aux = new Integer[op];
-        for(int i = 0; i<aux.length;i++){
+        for (int i = 0; i < aux.length; i++) {
             tam--;
             aux[i] = array[tam];
         }
